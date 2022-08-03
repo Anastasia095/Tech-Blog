ec11e00bc8643b30fdaacf29b76ddc4edc4c44e7
Anastasia Tarasenko <anastas.tarasenko001@mymdc.net>
2022-08-03 00:39:04 -0400

    first commit

---
+++ controllers/api/user-routes.js
@@ -0,0 +1,19 @@
+//require router, Post (from yout models), withAuth
+
+const { Router } = require("express");
+
+//get route that uses with Auth - uses findAll on posts
+//render all-posts-admin with the dashboard layout
+
+//get route for rendering the new post page, also using withAuth - findAll
+//render new-post with dashboard layout
+
+//get route for editigin a post, using with aith - findbypk
+
+router.get('edit/:id', withAuth. async (req, res) => {
+    try {
+        //await post data
+    } catch (err) {
+        //throughs error with a redirect to the login page
+    }
+})

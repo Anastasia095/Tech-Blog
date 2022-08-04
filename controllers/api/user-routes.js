96543ba45f1add080a37139a11ddceac39f8b27c
Anastasia Tarasenko <anastas.tarasenko001@mymdc.net>
2022-08-04 11:00:37 -0400

    created schema and model for plants

--- controllers/api/user-routes.js
+++ controllers/api/user-routes.js
@@ -1,7 +1,68 @@
 //require router, Post (from yout models), withAuth
 const router = require('express').Router();
 const { Post } = require('../../models');
+const { User } = require('../../models');
 const withAuth = require('../../utils/auth');
+
+//create User
+router.post('/', async (req, res) => {
+  try {
+    const userData = await User.create(req.body);
+
+    req.session.save(() => {
+      req.session.user_id = userData.id;
+      req.session.logged_in = true;
+
+      res.status(200).json(userData);
+    });
+  } catch (err) {
+    res.status(400).json(err);
+  }
+});
+//Login Route
+router.post('/login', async (req, res) => {
+  try {
+    const userData = await User.findOne({ where: { email: req.body.email } });
+
+    if (!userData) {
+      res
+        .status(400)
+        .json({ message: 'Incorrect email or password, please try again' });
+      return;
+    }
+
+    const validPassword = await userData.checkPassword(req.body.password);
+
+    if (!validPassword) {
+      res
+        .status(400)
+        .json({ message: 'Incorrect email or password, please try again' });
+      return;
+    }
+
+    req.session.save(() => {
+      req.session.user_id = userData.id;
+      req.session.logged_in = true;
+      
+      res.json({ user: userData, message: 'You are now logged in!' });
+    });
+
+  } catch (err) {
+    res.status(400).json(err);
+  }
+});
+
+//Logout route
+router.post('/logout', (req, res) => {
+  console.log("LOGOUT");
+  if (req.session.logged_in) {
+    req.session.destroy(() => {
+      res.status(204).end();
+    });
+  } else {
+    res.status(404).end();
+  }
+});
 
 //get route that uses with Auth - uses findAll on posts
 router.get('/',  withAuth, async (req, res) => {
